2b08099a654781ea6f34ac9e1973e9c8ad19c618
Austin Bentley <austinbentley@Austins-MacBook-Pro.local>
2019-01-19 20:11:16 -0700

    Updates

---
+++ precache-manifest.7549a18db17587f96e2dd761b7adbbf2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e5629f4c72d0d4a6bb9b35132e0f5f4",
+    "url": "/Portfolio/static/media/internshipfinal.8e5629f4.png"
+  },
+  {
+    "revision": "ab6bb9ec351431c627ed",
+    "url": "/Portfolio/static/js/main.ab6bb9ec.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "1fd7efcf5c7d0672b8597ec08aaec5c7",
+    "url": "/Portfolio/static/media/me.1fd7efcf.jpg"
+  },
+  {
+    "revision": "1fd2794e11cf02e1a47d2686959c2a07",
+    "url": "/Portfolio/static/media/tokyoghoul1.1fd2794e.png"
+  },
+  {
+    "revision": "7461a7b4cbca25f959fd4c85afec4bb9",
+    "url": "/Portfolio/static/media/tokyoghoul2.7461a7b4.jpg"
+  },
+  {
+    "revision": "0accb3ea798148c999b7febd90d1b2cb",
+    "url": "/Portfolio/static/media/RSILogoLarge.0accb3ea.png"
+  },
+  {
+    "revision": "c910c1c0cb0a8e271eb34b9d23678e17",
+    "url": "/Portfolio/static/media/NNMBackground.c910c1c0.jpg"
+  },
+  {
+    "revision": "9c7119123f7040d43593ba9db82be088",
+    "url": "/Portfolio/static/media/RheinTech.9c711912.png"
+  },
+  {
+    "revision": "c9f63e1b975d41cc73c8234878db380d",
+    "url": "/Portfolio/static/media/iot.c9f63e1b.jpg"
+  },
+  {
+    "revision": "7c248d554e39c2fb82cc",
+    "url": "/Portfolio/static/js/1.7c248d55.chunk.js"
+  },
+  {
+    "revision": "33c3051fa9d1d863c1ebff3ffef410b2",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.668b37baeaefe9c5672941161038ada9.js"
+  "/Portfolio/precache-manifest.7549a18db17587f96e2dd761b7adbbf2.js"
 );
 
 workbox.clientsClaim();
