6ea3f4a33a94c1ec95239215f00620bc62b9d7ba
Austin Bentley <australinsurf@gmail.com>
2020-05-29 15:33:38 -0600

    add svg for desktop

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 import MobileNameSVG from "../assets/MobileName.svg";
+import TabletNameSVG from "../assets/TabletName.svg";
 
 function Header() {
   return (
     <div className="header-section">
       <div
-        className="shapeshifter play name-svg"
+        className="shapeshifter play name-svg mobile"
         style={{ backgroundImage: `url(${MobileNameSVG})` }}
+      ></div>
+      <div
+        className="shapeshifter play name-svg tablet"
+        style={{ backgroundImage: `url(${TabletNameSVG})` }}
       ></div>
     </div>
   );
