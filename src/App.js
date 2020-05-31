f72b6dd1dd6170b95fdada9b6f84e36671c9ed83
Austin Bentley <australinsurf@gmail.com>
2020-05-31 15:13:49 -0600

    about me/skils sections started templating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Particles from "react-particles-js";
 import Header from "./sections/Header";
+import AboutMe from "./sections/AboutMe";
+import Skills from "./sections/Skills";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
+      <Skills />
       <Particles className="particle-background" />
     </div>
   );
---
+++ src/sections/Skills.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <div className="skills-section dp-section">
+      <h2 className="dp-title blue">Skills</h2>
+      <p className="dp-section--text">
+        I've worked with hundreds of toolkits/librarys/frameworks...etc. Here
+        are a few of the big ones.
+      </p>
+    </div>
+  );
+}
+
+export default Skills;
