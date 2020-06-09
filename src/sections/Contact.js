604d0bfa245ba20cbc476975951829bd1943e2ad
Austin Bentley <australinsurf@gmail.com>
2020-06-08 21:16:30 -0600

    commit to run deploy

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -7,8 +7,9 @@ function Contact() {
       <div className="contact-section dp-section">
         <h2 className="dp-title">Contact</h2>
         <p className="dp-section--text">
-          I'm open to remote opportunities with great culture. You can probably
-          get the fastest reply by sending me an email.
+          I'm open to remote opportunities with great culture. I enjoy fullstack
+          development using Elixir and React the most. You can probably get the
+          fastest reply by sending me an email.
         </p>
         <p className="dp-section--text">Email: australinsurf@gmail.com</p>
         <p className="dp-section--text">
