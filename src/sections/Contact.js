c41cd9cce2e58ca418398d58b2388a2b8cfd796b
Austin Bentley <australinsurf@gmail.com>
2020-07-03 14:14:08 -0600

    remove fade in on final div

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,23 +1,23 @@
 import React from "react";
-import FadeInSection from "../components/FadeInSection";
 
 function Contact() {
   return (
-    <FadeInSection>
-      <div className="contact-section dp-section">
-        <h2 className="dp-title">Contact</h2>
-        <p className="dp-section--text">
-          I'm open to remote opportunities with great culture. I enjoy fullstack
-          development using Elixir and React the most. You can probably get the
-          fastest reply by sending me an email.
-        </p>
-        <p className="dp-section--text">Email: australinsurf@gmail.com</p>
-        <p className="dp-section--text">
-          LinkedIn: https://www.linkedin.com/in/austinbentley/
-        </p>
-        <p className="dp-section--text">Residence: Albuquerque, NM</p>
-      </div>
-    </FadeInSection>
+    <div className="contact-section dp-section">
+      <h2 className="dp-title">Contact</h2>
+      <p className="dp-section--text">
+        I'm open to remote opportunities with great culture. I enjoy fullstack
+        development using Elixir and React the most. You can probably get the
+        fastest reply by sending me an email.
+      </p>
+      <p className="dp-section--text">Email: australinsurf@gmail.com</p>
+      <p className="dp-section--text">
+        LinkedIn:{" "}
+        <a href="https://www.linkedin.com/in/austinbentley/">
+          https://www.linkedin.com/in/austinbentley/
+        </a>
+      </p>
+      <p className="dp-section--text">Residence: Albuquerque, NM</p>
+    </div>
   );
 }
 
